ecc2185cfa4dcfc4e61608050809af530add18a2
vanlep <62962674+vanlep@users.noreply.github.com>
2020-04-08 10:17:34 +0200

    Update exo_07_c.rb

--- exo_07_c.rb
+++ exo_07_c.rb
@@ -1,2 +1,4 @@
 user_name = gets.chomp
 puts user_name
+
+#la difference est dans la "mise en page", la a va demander le nom, laissé l'utilisateur repondre et ecrire le nom, la b va demande le nom mettre ">" et reecrire le nom, la c va juste laisser un blanc et reecrire ce qui va etre ecrit dans la blanc
