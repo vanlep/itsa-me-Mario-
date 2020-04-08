2485f333442406660248435217c1f142de62890d
vanlep <62962674+vanlep@users.noreply.github.com>
2020-04-08 10:24:01 +0200

    Update exo_05.rb

--- exo_05.rb
+++ exo_05.rb
@@ -1,20 +1,21 @@
-elle affiche une phrase puts "On va compter le nombre d'heures de travail a THP"
-elle reponde a la question precedente puts "Travail : #{10 * 5 * 11}"
-elle donne la reponse en minute puts "En minuts ca fait : #{10 * 5 * 11 * 60}"
+puts "On va compter le nombre d'heures de travail a THP"  #elle affiche une phrase
+puts "Travail : #{10 * 5 * 11}"  #elle reponde a la question precedente 
+puts "En minuts ca fait : #{10 * 5 * 11 * 60}" #elle donne la reponse en minute 
 
-pose la question en seconde puts "Et en seconde ?"
+puts "Et en seconde ?" #pose la question en seconde 
 
-repond a la quesion puts 10 * 5 * 11 * 60 * 60
+puts 10 * 5 * 11 * 60 * 60 #repond a la quesion 
 
-pose une question puts "Est ce que c'est vrai que 3 + 2 < 5 - 7?"
 
-repond a la question puts 3 + 2 < 5 - 7
+puts "Est ce que c'est vrai que 3 + 2 < 5 - 7?" #pose une question 
 
-pose un calcul puts "Ca fait combien 3 + 2 ? #{3 + 2}"
-pose un calcul puts "Ca fait combien 5 - 7 ? #{5 - 7}"
+puts 3 + 2 < 5 - 7 #repond a la question 
 
-repond a la question puts "Ok, c'est faux alors !"
+puts "Ca fait combien 3 + 2 ? #{3 + 2}" #pose un calcul 
+puts "Ca fait combien 5 - 7 ? #{5 - 7}" #pose un calcul 
 
-pose et repond a une question de calcul puts "Est ce que 5 est plus grand que -2 ? #{5 > -2}"
-pose et repond a une question de calcul puts "Est ce que 5 est superieur ou egal a -2 ? #{5 >= -2}"
-pose et repond a une question de calcul puts "Est ce que 5 est inferieur ou egal a -2 ? #{5 <= -2}"
+puts "Ok, c'est faux alors !" #repond a la question 
+
+puts "Est ce que 5 est plus grand que -2 ? #{5 > -2}" #pose et repond a une question de calcul 
+puts "Est ce que 5 est superieur ou egal a -2 ? #{5 >= -2}" #pose et repond a une question de calcul 
+puts "Est ce que 5 est inferieur ou egal a -2 ? #{5 <= -2}" #pose et repond a une question de calcul 
