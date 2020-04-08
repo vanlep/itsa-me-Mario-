3a864bbef79863a3465d51eb988de4799bf19342
vanlep <62962674+vanlep@users.noreply.github.com>
2020-04-08 10:20:20 +0200

    Update exo_06.rb

--- exo_06.rb
+++ exo_06.rb
@@ -5,3 +5,5 @@ number_of_weeks_worked_in_THP = 11
 puts "Travail : #{number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_worked_in_THP}"
 
 puts "Et en minute ca fait #{number_of_minuts_in_an_hour * number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_worked_in_THP}"
+
+#le programme affiche une erreur car il ne connait pas la valeur de "number_of_minute_in_an_hour"
